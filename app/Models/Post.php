e713286f427b37d97bcd5bf07f3768d925a6592f
MIqbalNP <miqbalnp@students.amikom.ac.id>
2026-01-28 16:26:59 +0700

    Modify Model Post & User

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,23 @@ class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'content',
+        'is_draft',
+        'published_at',
+    ];
+
+    public function scopePublished(Builder $query): void
+    {
+        $query->where('is_draft', false)
+            ->where('published_at', '<=', now());
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
