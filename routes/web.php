17b7633c946207bc669761995084a3a936c3bd62
MIqbalNP <miqbalnp@students.amikom.ac.id>
2026-01-28 16:27:42 +0700

    add Post Request, Polcy & route Web

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Post\PostController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -12,6 +13,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+
+// Butuh Auth
+Route::resource('posts', PostController::class)->except(['index', 'show'])->middleware('auth');
+// Publik
+Route::resource('posts', PostController::class)->only(['index', 'show']);
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
